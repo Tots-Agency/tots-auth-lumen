75929260dc346d982715b712b2576b77be42fff7
Matias Camiletti <matias.camiletti@gmail.com>
2022-11-26 18:34:03 -0300

    Add user model.

---
+++ src/Models/TotsUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tots\Auth\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class TotsUser extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['email'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var string[]
+     */
+    protected $hidden = ['password'];
+}
