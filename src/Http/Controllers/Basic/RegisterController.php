4ae6554838eba8bc48278d0bf22121ec0a8a8eff
Matias Camiletti <matias.camiletti@gmail.com>
2022-11-26 20:12:46 -0300

    Add register controller.

---
+++ src/Http/Controllers/Basic/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tots\Auth\Http\Controllers\Basic;
+
+use Illuminate\Http\Request;
+use Tots\Auth\Models\TotsUser;
+use Illuminate\Support\Facades\Hash;
+use Tots\Auth\Services\AuthService;
+
+class RegisterController extends \Laravel\Lumen\Routing\Controller
+{
+
+    /**
+     *
+     * @var AuthService
+     */
+    protected $service;
+
+    public function __construct(AuthService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function handle(Request $request)
+    {
+        // Validation
+        $this->validate($request, [
+            'password' => 'required|min:6',
+            'email' => 'required|email|unique:tots_user'
+        ]);
+        // Get Params
+        $email = $request->input('email');
+        $password = $request->input('password');
+        // Create new user
+        $user = new TotsUser();
+        //$user->firstname = $request->input('firstname');
+        //$user->lastname = $request->input('lastname');
+        $user->email = $email;
+        $user->password = Hash::make($password);
+        $user->save();
+        // Return data
+        return $user;
+    }
+}
